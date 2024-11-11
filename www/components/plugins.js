c0c790f2c2e79e5c4a2f42b1bacc9352b736f99b
PradoDK <lucaspradoalves75@gmail.com>
2024-11-11 11:37:45 +0900

    gf

--- www/components/plugins.js
+++ www/components/plugins.js
@@ -1,2 +1,19 @@
 // This is a JavaScript file
 
+$(document).on("click", "#camera",function(){
+
+navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
+    destinationType: Camera.DestinationType.FILE_URI });
+
+function onSuccess(imageURI) {
+    window.resolveLocalFileSystemURL(uri, (entry) => {
+        let img = document.getElementById('image');
+        img.src = entry.toURL();
+    }, onFail);
+}
+
+function onFail(message) {
+    alert('Failed because: ' + message);
+}
+
+});
